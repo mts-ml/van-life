8fe986a9c0cbba69ebb93839127da99c0f4459cb
Mateus <mtsmlima@gmail.com>
2025-02-07 17:54:59 -0300

    Imported makeServer and added logic

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
 import App from './App'
+import { makeServer } from "./server";
+
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
 
 
 createRoot(document.querySelector('#root')).render(

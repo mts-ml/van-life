b5eea82b17aa2a016f2cef551fc5f2fe6b63a815
Mateus <mtsmlima@gmail.com>
2025-02-07 17:54:36 -0300

    Changed import and route

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import Vans from './components/Vans/Vans';
+import VanDetail from './components/VanDetail/VanDetail';
 import { Route, Routes } from 'react-router-dom';
 
 import './style/globalStyle.scss';
-import Vans from './components/Vans/Vans';
 
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
 
       <Footer />

b59fca71ef3fb57ff3d95ecf529be6b14542658f
Mateus <mtsmlima@gmail.com>
2025-02-10 19:16:13 -0300

    Changed routes and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Home from './components/Home/Home';
-import About from './components/About/About';
-import Vans from './components/Vans/Vans';
-import VanDetail from './components/VanDetail/VanDetail';
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Vans from './pages/Vans/Vans';
+import VanDetail from './pages/VanDetail/VanDetail';
 import { Route, Routes } from 'react-router-dom';
+import HostLayout from './components/HostLayout/HostLayout'
+import Dashboard from './pages/Dashboard/Dashboard';
+import Income from './pages/Income/Income';
+import HostVans from './pages/HostVans/HostVans';
+import Reviews from './pages/Reviews/Reviews';
 
 import './style/globalStyle.scss';
 
 
 function App() {
   return (
-    <>
-      <Header />
-
-      <Routes>
+    <Routes>
+      <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
+        <Route path="about" element={<About />} />
+        <Route path="vans" element={<Vans />} />
+        <Route path="vans/:id" element={<VanDetail />} />
+        <Route path="host" element={<HostLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="income" element={<Income />} />
+          <Route path="vans" element={<HostVans />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
 
-        <Route path="/vans/:id" element={<VanDetail />} />
-      </Routes>
-
-      <Footer />
-    </>
+      </Route>
+    </Routes >
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import logo from '../../assets/images/logo.svg';
 
 import './headerStyle.scss';
 
 
 export default function Header() {
+
    return (
       <header className="header">
          <div className="header__wrapper">
@@ -17,15 +18,34 @@ export default function Header() {
             <nav>
                <ul className="header__list">
                   <li>
-                     <Link to="/about" className='header__link'>About</Link>
+                     <NavLink
+                        to="/host"
+                        className={({ isActive }) => `header__link ${isActive ? "active-link" : null}`}
+                     >
+                        Host
+                     </NavLink>
                   </li>
 
                   <li>
-                     <Link to="/vans" className='header__link'>Vans</Link>
+                     <NavLink
+                        to="/about"
+                        className={({isActive}) => `header__link ${isActive ? "active-link" : null}`}
+                     >
+                        About
+                     </NavLink>
+                  </li>
+
+                  <li>
+                     <NavLink
+                        to="/vans"
+                        className={({isActive}) => `header__link ${isActive ? "active-link" : null}`}
+                     >
+                        Vans
+                     </NavLink>
                   </li>
                </ul>
             </nav>
          </div>
-      </header >
+      </header>
    )
 }

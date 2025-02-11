f67edbada64fa33abb700cdfbbb0273c22321623
Mateus <mtsmlima@gmail.com>
2025-02-11 19:10:19 -0300

    Fixed how footer was displayed on host/vans page.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,16 @@
-import './footerStyle.scss'
+import {useLocation} from 'react-router-dom';
+
+import './footerStyle.scss';
 
 
 export default function Footer() {
+   const location = useLocation();
+
+   const isHostVansPage = location.pathname === '/host/vans';
+
+
    return (
-      <footer className='footer'>
+      <footer className={`footer ${isHostVansPage ? 'fixed-footer' : ''}`}>
          <h3 className='footer__title'>Ⓒ 2022 #VANLIFE</h3>
       </footer>
    )

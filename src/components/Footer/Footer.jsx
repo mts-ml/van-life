fe39e1ade52bc48c34868910782fd5b8145f1b25
Mateus <mtsmlima@gmail.com>
2025-02-12 15:54:35 -0300

    Added logic to make footer fixed or static based on screen size

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,40 @@
-import {useLocation} from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 import './footerStyle.scss';
 
 
 export default function Footer() {
-   const location = useLocation();
 
-   const isHostVansPage = location.pathname === '/host/vans';
+   const location = useLocation();
+   
+   const [isScrollable, setIsScrollable] = useState(false);
+
+   useEffect(() => {
+      const checkScrollable = () => {
+         setIsScrollable(document.documentElement.scrollHeight > window.innerHeight);
+      };
+
+      // Verifies everytime the page renders
+      checkScrollable();
+
+      // Uses ResizeObserver to detect changed on the content para detectar mudanças na altura do conteúdo
+      const observer = new ResizeObserver(checkScrollable);
+      observer.observe(document.body);
+
+      window.addEventListener('resize', checkScrollable);
+
+      return () => {
+         window.removeEventListener('resize', checkScrollable);
+         observer.disconnect();
+      };
+   }, [location.pathname]);
+
 
 
    return (
-      <footer className={`footer ${isHostVansPage ? 'fixed-footer' : ''}`}>
+      <footer className={`footer ${isScrollable ? '' : 'fixed-footer'}`}>
          <h3 className='footer__title'>Ⓒ 2022 #VANLIFE</h3>
       </footer>
-   )
+   );
 }

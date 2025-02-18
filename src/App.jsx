a74a1768c051bd2b30e90007a47b3150099794a9
Mateus <mtsmlima@gmail.com>
2025-02-18 16:13:33 -0300

    Changed how the routes are setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home/Home';
-import About from './pages/Host/About/About';
-import Vans from './pages/Vans/Vans';
+import About from './pages/About/About';
+import Vans, { loader as vansLoader } from './pages/Vans/Vans';
 import VanDetail from './pages/VanDetail/VanDetail';
-import { Route, Routes } from 'react-router-dom';
 import HostLayout from './components/HostLayout/HostLayout'
 import Dashboard from './pages/Host/Dashboard/Dashboard';
 import Income from './pages/Host/Income/Income';
@@ -13,34 +12,48 @@ import Reviews from './pages/Host/Reviews/Reviews';
 import HostVansInfo from './pages/Host/HostVansDetail/HostVansInfo/HostVansInfo';
 import HostVansPricing from './pages/Host/HostVansDetail/HostVansPricing/HostVansPricing';
 import HostVansPhotos from './pages/Host/HostVansDetail/HostVansPhotos/HostVansPhotos';
-
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import ErrorRequest from './components/ErrorRequest/ErrorRequest';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { makeServer } from "./server";
 
 import './style/globalStyle.scss';
 
 
+makeServer();
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route element={<Layout />}>
+    <Route path="/" element={<Home />} />
+    <Route path="about" element={<About />} />
+    <Route
+      path="vans"
+      element={<Vans />}
+      loader={vansLoader}
+      errorElement={<ErrorRequest />}
+    />
+    <Route path="vans/:id" element={<VanDetail />} />
+
+    <Route path="host" element={<HostLayout />}>
+      <Route index element={<Dashboard />} />
+      <Route path="income" element={<Income />} />
+      <Route path="reviews" element={<Reviews />} />
+      <Route path="vans" element={<HostVans />} />
+
+      <Route path="vans/:id" element={<HostVansDetail />}>
+        <Route index element={<HostVansInfo />} />
+        <Route path="pricing" element={<HostVansPricing />} />
+        <Route path="photos" element={<HostVansPhotos />} />
+      </Route>
+    </Route>
+    <Route path="*" element={<ErrorPage />} />
+  </Route>
+))
+
+
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="vans" element={<Vans />} />
-        <Route path="vans/:id" element={<VanDetail />} />
-
-        <Route path="host" element={<HostLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="income" element={<Income />} />
-          <Route path="reviews" element={<Reviews />} />
-          <Route path="vans" element={<HostVans />} />
-
-          <Route path="vans/:id" element={<HostVansDetail />}>
-            <Route index element={<HostVansInfo />} />
-            <Route path="pricing" element={<HostVansPricing />} />
-            <Route path="photos" element={<HostVansPhotos />} />
-          </Route>
-        </Route>
-      </Route>
-    </Routes >
+    <RouterProvider router={router} />
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,7 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App'
-import { makeServer } from "./server";
 
-
-makeServer();
 
 createRoot(document.querySelector('#root')).render(
-  <BrowserRouter>
     <App />
-  </BrowserRouter>
 );
